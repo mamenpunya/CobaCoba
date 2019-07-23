0e39be125bc23a3418682d36e0bb9de632f7e132
Rully <mamenpunya21@gmail.com>
2019-07-23 19:53:04 +0700

    ini

---
+++ app/src/main/java/com/dicoding/mamen/cobacoba/MainActivity.java
@@ -0,0 +1,27 @@
+package com.dicoding.mamen.cobacoba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.dicoding.mamen.cobacoba.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.togog);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+    }
+}
